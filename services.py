2b3c96610fcd69e54003ab4bd5c6613858b77ec4
XAM MAAGAD <85304249+xamjyss020820@users.noreply.github.com>
2021-08-04 14:48:58 +0800

    Update services.py

--- services.py
+++ services.py
@@ -14,8 +14,8 @@ PASS = ''
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:990'
-RESPONSE = 'HTTP/1.1 101 <font color="red">xamjyssvpn.com & coronassh.com</font>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
+DEFAULT_HOST = '127.0.0.1:900'
+RESPONSE = 'HTTP/1.1 101 <font color="red">xamjyssvpn.com</font>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
